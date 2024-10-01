e701043edfe0913d1d5560f19723a4532a3d3eac
BPunsara <bpunsara@jayjaymills.lk>
2024-10-01 16:49:57 +0530

    database connection created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { SocialController } from './social/social.controller';
+import { SocialService } from './social/social.service';
+import { SocialModule } from './social/social.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+
+      ],
+      synchronize: true,
+      logging: false
+    }),
+    SocialModule,
+  ],
+  controllers: [AppController, SocialController],
+  providers: [AppService, SocialService],
 })
 export class AppModule {}
