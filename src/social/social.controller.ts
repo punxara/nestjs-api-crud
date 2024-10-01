63f3af488d7eb215ff7e071aa6d954f4f369bc5f
BPunsara <bpunsara@jayjaymills.lk>
2024-10-01 16:58:15 +0530

    social controller and services for create, read, update done

---
+++ src/social/social.controller.ts
@@ -0,0 +1,37 @@
+import {Body, Controller, Get, Param, Post, Put} from "@nestjs/common";
+import {SocialService} from "./social.service";
+import {Social} from "./social.entity";
+
+@Controller("social")
+export class SocialController {
+
+    constructor(
+        private service: SocialService
+    ) {
+    }
+
+    @Post()
+    create(@Body() item: Social): Promise<Social> {
+        return this.service.create(item);
+    }
+
+    @Put(":id")
+    async update(@Param("id") id: number, @Body() item: Social): Promise<Social> {
+        return await this.service.update(id, item);
+    }
+
+    @Put("change-publicity/:id")
+    async changePublicity(@Param("id") id: number, @Body() isPublic: {}): Promise<Social> {
+        return await this.service.changePublicity(id, isPublic);
+    }
+
+    @Get('get-by-id/:id')
+    async get(@Param('id') id: string): Promise<Social> {
+        return await this.service.get(+id);
+    }
+
+    @Get('get-all-socials')
+    getAll(): Promise<Social[]> {
+        return this.service.getAll();
+    }
+}
---
+++ src/social/social.service.ts
@@ -0,0 +1,74 @@
+import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
+import {EntityManager} from "typeorm";
+import {Social} from "./social.entity";
+import {SocialDto} from "./dto/social.dto";
+
+@Injectable()
+export class SocialService {
+
+    constructor(
+        private entityManager: EntityManager,
+    ) {
+    }
+
+    async create(item: Social): Promise<SocialDto> {
+        const entity: Social = new Social();
+        entity.platform = item.platform;
+        entity.username = item.username;
+        entity.link = item.link;
+        entity.isPublic = 1;
+        return await this.entityManager.save(entity);
+    }
+
+    async update(id: number, item: Social): Promise<SocialDto> {
+
+        const entity: Social = await this.entityManager.findOneBy(Social, {id});
+        if (!entity) {
+            throw new HttpException(`Sorry, movie doesn't exist.`, HttpStatus.BAD_REQUEST,);
+        }
+
+        entity.platform = item.platform;
+        entity.username = item.username;
+        entity.link = item.link;
+        entity.isPublic = item.isPublic;
+
+        try {
+            return await this.entityManager.transaction(async transactionalEntityManager => {
+                    return await transactionalEntityManager.save(entity);
+                }
+            );
+        } catch (err) {
+            throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    async changePublicity(id: number, isPublic: {}): Promise<SocialDto> {
+
+        const entity: Social = await this.entityManager.findOneBy(Social, {id});
+        if (!entity) {
+            throw new HttpException(`Sorry, movie doesn't exist.`, HttpStatus.BAD_REQUEST);
+        }
+
+        if (isPublic['isPublic'] === 0) {
+            entity.isPublic = 1;
+        } else if (isPublic['isPublic'] === 1) {
+            entity.isPublic = 0;
+        }
+        try {
+            return await this.entityManager.transaction(async transactionalEntityManager => {
+                    return await transactionalEntityManager.save(entity);
+                }
+            );
+        } catch (err) {
+            throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    async get(id: number): Promise<SocialDto> {
+        return await this.entityManager.findOneBy(Social, {id: id});
+    }
+
+    async getAll(): Promise<SocialDto[]> {
+        return await this.entityManager.find(Social);
+    }
+}
